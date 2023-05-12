8957b9e11a9d83a668810ec0fc1241fbc36f7c3c
ashtron <ashton.playsted@protonmail.com>
2023-05-11 19:01:57 -0700

    Add basic (ugly) autocomplete search bar component

--- app/components/form/search-bar.js
+++ app/components/form/search-bar.js
@@ -1,28 +1,56 @@
 "use client";
 
 import "./search-bar.css";
+import { useState, useEffect } from "react";
 
 export default function SearchBar() {
-  function handleKeyDown(event) {
+  const [books, setBooks] = useState([]);
+  const [booksLoaded, setBooksLoaded] = useState(false);
+  const [text, setText] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+
+  const handleChange = (event) => {
     event.preventDefault();
 
-    if (event.keyCode === 13) {
-      console.log("yep")
+    const newText = event.target.value;
+
+    setText(newText);
+  };
+
+  useEffect(() => {
+    const loadBooks = async () => {
+      let books = await fetch("https://jsonplaceholder.typicode.com/albums");
+      let parsedBooks = await books.json();
+
+      setBooks(parsedBooks.slice(0, 7));
+      setBooksLoaded(true);
+    };
+
+    if (!booksLoaded) loadBooks();
+  });
+
+  useEffect(() => {
+    let matches = [];
+
+    if (text.length > 0) {
+      matches = books.filter((book) => {
+        const regex = new RegExp(`${text}`, "gi");
+        return book.title.match(regex);
+      });
     }
-  }
+
+    setSuggestions(matches);
+  }, [text]);
 
   return (
-    <div className="search-box">
-      <div className="row">
-        <input type="text" id="input-box" autocomplete="off" onKeyDown={handleKeyDown} />
-        <div className="results">
-          <ul>
-            <li>One</li>
-            <li>And another</li>
-          </ul>
-        </div>
-        <button>Search</button>
-      </div>
+    <div className="search-bar">
+      <input type="text" onChange={handleChange} value={text} />
+      <ul>
+        {suggestions &&
+          suggestions.map((suggestion, i) => {
+            return <li key={i}>{suggestion.title}</li>;
+          })}
+      </ul>
     </div>
   );
 }
