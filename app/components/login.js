c4cc5f006ed7ddc77cdf378b1abbc09ec5d5e9f1
ashtron <ashton.playsted@protonmail.com>
2023-06-15 14:25:07 -0700

    Add login functionality to LogIn component

--- app/components/login.js
+++ app/components/login.js
@@ -1,18 +1,62 @@
+"use client";
+
 import Link from "next/link";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function LogIn() {
+  const supabase = createBrowserSupabaseClient();
+  const router = useRouter();
+
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
+
+  async function logIn(event) {
+    event.preventDefault();
+
+    await supabase.auth.signInWithPassword(credentials);
+
+    router.push("/media");
+  }
+
+  function handleChange(event) {
+    event.preventDefault();
+
+    setCredentials({
+      ...credentials,
+      [event.target.name]: event.target.value,
+    });
+  }
+
   return (
     <main className="container">
-      <article>
-        <header>Log In or {<Link href="/sign-up">Create an Account</Link>}</header>
-        <label htmlFor="username">Username</label>
-        <input type="text" name="username" />
+      <form onSubmit={logIn}>
+        <article>
+          <header>
+            Log In or {<Link href="/sign-up">Create an Account</Link>}
+          </header>
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            name="email"
+            onChange={handleChange}
+            value={credentials.email}
+          />
 
-        <label htmlFor="password">Password</label>
-        <input type="password" name="password" />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            onChange={handleChange}
+            value={credentials.password}
+          />
 
-        <button type="submit">Log In</button>
-      </article>
+          <button type="submit">Log In</button>
+        </article>
+      </form>
     </main>
   );
 }
